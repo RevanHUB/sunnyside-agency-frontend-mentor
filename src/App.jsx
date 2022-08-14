b94120c604c7f05fe85fa8d74b32b5114729c43e
David <davidasir2020@gmail.com>
2022-08-14 13:48:34 +0000

    chore: updated to compatibility to vercel

--- src/App.jsx
+++ src/App.jsx
@@ -27,24 +27,24 @@ class App extends React.Component {
         name: 'Emily R.',
         pos: 'Marketing Director',
         desc: 'We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.',
-        img: './src/assets/resources/image-emily.jpg'
+        img: '../src/assets/resources/image-emily.jpg'
       },
       {
         id: 1,
         name: 'Thomas S.',
         pos: 'Chief Operating Officer',
         desc: "Sunnyside's enthusiasm coupled with their keen interest in our brand's success made it a satisfying and enjoyable experience.",
-        img: './src/assets/resources/image-thomas.jpg'
+        img: '../src/assets/resources/image-thomas.jpg'
       },
       {
         id: 2,
         name: 'Jennie F.',
         pos: 'Business Owner',
         desc: 'Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!',
-        img: './src/assets/resources/image-jennie.jpg'
+        img: '../src/assets/resources/image-jennie.jpg'
       },
     ],
-    this.gallery = ['./src/assets/resources/desktop/image-gallery-milkbottles.jpg', './src/assets/resources/desktop/image-gallery-orange.jpg', './src/assets/resources/desktop/image-gallery-cone.jpg', './src/assets/resources/desktop/image-gallery-sugarcubes.jpg' ]
+    this.gallery = ['../src/assets/resources/desktop/image-gallery-milkbottles.jpg', '../src/assets/resources/desktop/image-gallery-orange.jpg', '../src/assets/resources/desktop/image-gallery-cone.jpg', '../src/assets/resources/desktop/image-gallery-sugarcubes.jpg' ]
   };
   render() {
     
@@ -60,7 +60,7 @@ class App extends React.Component {
               title="Transform your brand"
               desc=" We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you."
               order="right"
-              img_path="./src/assets/resources/desktop/image-transform.jpg" 
+              img_path="../src/assets/resources/desktop/image-transform.jpg" 
           />
          
           <SectionRight
@@ -68,16 +68,16 @@ class App extends React.Component {
               title="Stand out to the right audience"
               desc="Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we'll build and extend your brand in digital places."
               order="left"
-              img_path="./src/assets/resources/desktop/image-stand-out.jpg" 
+              img_path="../src/assets/resources/desktop/image-stand-out.jpg" 
           />
           <SectionDouble
             id="section__3"
             title_l="Graphic Design"
             desc_l="Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential client's attention."
-            img_path_l="./src/assets/resources/desktop/image-graphic-design.jpg" 
+            img_path_l="../src/assets/resources/desktop/image-graphic-design.jpg" 
             title_r="Photography"
             desc_r="Increase your credibility by getting the most stunning, high-quality photos that improve your business image."
-            img_path_r="./src/assets/resources/desktop/image-photography.jpg" 
+            img_path_r="../src/assets/resources/desktop/image-photography.jpg" 
           />
           <Testimonials
             id="testimonials"
